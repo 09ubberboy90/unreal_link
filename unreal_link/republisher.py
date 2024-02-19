effadd0ccddcd4a865e0f73ed3a0822cf4d907d2
Florent Audonnet <2330834a@student.gla.ac.uk>
2024-02-19 14:08:19 +0900

    remove depracated code

--- unreal_link/republisher.py
+++ unreal_link/republisher.py
@@ -12,33 +12,11 @@ class MinimalPublisher(Node):
         super().__init__('republisher')
         self.publisher_ = self.create_publisher(Bool, 'right_hand/trigger', 10)
         self.sub_ = self.create_subscription(EBool, 'bp/right_hand/trigger',self.listener_callback, 10)
-        # self.sub2_ = self.create_subscription(JointState, 'joint_states_sim_right',self.joint_callback, 10)
-        # self.publisher2_ = self.create_publisher(JointState, 'gripper_states_sim', 10)
-        self.gripper_joints = [
-            "finger_joint",
-            "left_inner_knuckle_joint",
-            "right_inner_knuckle_joint",
-            "right_outer_knuckle_joint",
-            "left_inner_finger_joint",
-            "right_inner_finger_joint",
-        ]
+
     def listener_callback(self, msg):
         new = Bool()
         new.data = msg.data
         self.publisher_.publish(new) 
-    def joint_callback(self, msg):
-        idxs = {}
-        for idx, name in enumerate(msg.name):
-            if name in self.gripper_joints:
-                idxs[name]= idx
-        pos = []
-        for name in self.gripper_joints:
-            pos.append(msg.position[idxs[name]])
-        new = JointState()
-        new.header = msg.header
-        new.name = self.gripper_joints
-        new.position = pos
-        self.publisher2_.publish(new)
         
 def main(args=None):
     rclpy.init(args=args)
