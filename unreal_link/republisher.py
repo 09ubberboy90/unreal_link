6248e3e5213ce70bb338870e2a8a619f3edf47a8
09ubberboy90 <2330834a@student.gla.ac.uk>
2024-03-20 17:51:59 +0000

    Add for Baxter

--- unreal_link/republisher.py
+++ unreal_link/republisher.py
@@ -11,12 +11,20 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('republisher')
         self.publisher_ = self.create_publisher(Bool, 'right_hand/trigger', 10)
-        self.sub_ = self.create_subscription(EBool, 'bp/right_hand/trigger',self.listener_callback, 10)
+        self.sub_ = self.create_subscription(EBool, 'bp/baxter/right_hand/trigger',self.listener_callback, 10)
+        self.publisher2_ = self.create_publisher(Bool, 'left_hand/trigger', 10)
+        self.sub2_ = self.create_subscription(EBool, 'bp/baxter/left_hand/trigger',self.listener_callback2, 10)
+        print("Republisher started")
 
     def listener_callback(self, msg):
         new = Bool()
         new.data = msg.data
         self.publisher_.publish(new) 
+
+    def listener_callback2(self, msg):
+        new = Bool()
+        new.data = msg.data
+        self.publisher2_.publish(new) 
         
 def main(args=None):
     rclpy.init(args=args)
